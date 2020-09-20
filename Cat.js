3059d0b6dbbf2e5a601bef2a5aecbb309d50ba46
Elon103 <minhson031094@gmail.com>
2020-09-20 22:03:55 +0700

    Dog class

--- Cat.js
+++ Cat.js
@@ -2,8 +2,12 @@ function Cat(name){
     this.name = name;
     this.dead = false;
 }
+
 Cat.prototype.die = function(){
     this.dead = true;
-}
+};
+Cat.prototype.eat = function(){
+    
+};
 module.export = Cat;
 //comment sadfasdf
